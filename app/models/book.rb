a903b34ed6dcc6b36fb42442044aae4c61f89014
Roman Korobko <romakorobko@gmail.com>
2018-10-18 16:47:03 +0300

    require cloudinary and picture to books

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,6 @@ class Book < ApplicationRecord
   has_many :book_authors
   has_many :authors, through: :book_authors
   has_many :reviews
+
+  mount_uploader :picture, PictureUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_17_123809) do
+ActiveRecord::Schema.define(version: 2018_10_18_134019) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2018_10_17_123809) do
     t.string "dimensions"
     t.string "materials"
     t.integer "category_id"
+    t.string "picture"
   end
 
   create_table "categories", force: :cascade do |t|
