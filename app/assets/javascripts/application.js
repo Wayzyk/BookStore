be4afe1c6a5d25388e87af3bdba113ff27f641fa
Roman Korobko <romakorobko@gmail.com>
2018-10-18 17:44:44 +0300

    fix jquery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require jquery
+//= require jquery2
 //= require bootstrap-sprockets
 //= require activestorage
 //= require turbolinks
