9a7feb16f50fa4f54a5e6de39f31e3ef7ac79317
Roman Korobko <romakorobko@gmail.com>
2018-10-19 19:48:10 +0300

    fix filters for books

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   resources :categories do
-    resources :books, only: :index 
+    resources :books, only: :index
   end
 
-resources :books, only: :show do
-  resources :reviews
-end
+  resources :books, only: :show do
+    resources :reviews
+  end
+
   root 'pages#home'
 end
