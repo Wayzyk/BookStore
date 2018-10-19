320a98cbad2ddb06f1d8c4739015e8a86b86a69c
Roman Korobko <romakorobko@gmail.com>
2018-10-19 09:14:08 +0300

    create relate between books and categories

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,7 @@ class Book < ApplicationRecord
   has_many :book_authors
   has_many :authors, through: :book_authors
   has_many :reviews
-
+  has_many :book_categories
+  has_many :categories, through: :book_categories
   mount_uploader :picture, PictureUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_18_134019) do
+ActiveRecord::Schema.define(version: 2018_10_19_061100) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,13 @@ ActiveRecord::Schema.define(version: 2018_10_18_134019) do
     t.index ["book_id"], name: "index_book_authors_on_book_id"
   end
 
+  create_table "book_categories", force: :cascade do |t|
+    t.bigint "book_id"
+    t.bigint "category_id"
+    t.index ["book_id"], name: "index_book_categories_on_book_id"
+    t.index ["category_id"], name: "index_book_categories_on_category_id"
+  end
+
   create_table "books", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -37,7 +44,6 @@ ActiveRecord::Schema.define(version: 2018_10_18_134019) do
     t.date "year_of_publish"
     t.string "dimensions"
     t.string "materials"
-    t.integer "category_id"
     t.string "picture"
   end
 
