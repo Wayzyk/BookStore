4285333bb1b1b47209189fb8f79dad7828235dac
Roman Korobko <romakorobko@gmail.com>
2018-10-19 13:17:55 +0300

    add code

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,6 +2,7 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.page(params[:page])
+    @filter = Category.find_by(name: Category.name)
   end
 
   def show
--- app/models/book.rb
+++ app/models/book.rb
@@ -3,6 +3,6 @@ class Book < ApplicationRecord
   has_many :authors, through: :book_authors
   has_many :reviews
   has_many :book_categories
-  has_many :categories, through: :book_categories
+  belongs_to :categories, through: :book_categories
   mount_uploader :picture, PictureUploader
 end
