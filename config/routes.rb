09c38e80032cdde9bda551d23b9b2c145b6b7255
Roman Korobko <romakorobko@gmail.com>
2018-10-19 17:54:21 +0300

    change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
-  resources :books do
-    resources :reviews
+  resources :categories do
+    resources :books, only: :index 
   end
+
+resources :books, only: :show do
+  resources :reviews
+end
   root 'pages#home'
 end
