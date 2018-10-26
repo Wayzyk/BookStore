747bb05e538dcaf9092bc8e4f272530307ee176c
Roman Korobko <romakorobko@gmail.com>
2018-10-26 17:05:14 +0300

    add seeds

--- factories.rb
+++ factories.rb
@@ -2,5 +2,13 @@ FactoryBot.define do
 
   factory :book do
     name { Faker::Book.title }
+    price { Faker::Number.decimal(2) }
+    description { Faker::Food.description }
+    year_of_publish { Faker::Stripe.year }
+  end
+
+  factory :author do
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
   end
 end
