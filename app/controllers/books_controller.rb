65777561f88d83ff9a32d5455e71021fd5610fb4
Roman Korobko <romakorobko@gmail.com>
2018-10-29 15:11:23 +0200

    fix error with pagination and categories

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,13 +2,11 @@ class BooksController < ApplicationController
 
   def index
     if params[:category].blank?
-      @books = Book.page(params[:page]).per(12)
-      # Kaminari.paginate_array(@books).page(params[:page]).per(12)
+      @books = Book.all
     else
-      @books = Book.joins(:categories).where(categories: { name: params[:category] })
+      @books = Book.page(params[:page]).per(12).joins(:categories).where(categories: { name: params[:category] })
     end
     @order_item = current_order.order_items.new
-    render action: :index, layout: request.xhr? == nil
   end
 
   def show
