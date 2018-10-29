5d041f8b19d2b06f527176715536f1098031331d
Roman Korobko <romakorobko@gmail.com>
2018-10-29 13:55:53 +0200

    add pagination with kaminari

--- Gemfile
+++ Gemfile
@@ -55,6 +55,7 @@ gem "figaro"
 
 gem 'faker'
 
+gem 'kaminari'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,11 +2,13 @@ class BooksController < ApplicationController
 
   def index
     if params[:category].blank?
-      @books = Book.all
+      @books = Book.page(params[:page]).per(12)
+      # Kaminari.paginate_array(@books).page(params[:page]).per(12)
     else
       @books = Book.joins(:categories).where(categories: { name: params[:category] })
     end
     @order_item = current_order.order_items.new
+    render action: :index, layout: request.xhr? == nil
   end
 
   def show
