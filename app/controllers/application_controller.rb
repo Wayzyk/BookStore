0571b70b02480e47ba48b296ef7105cf26b3fd4a
Roman Korobko <romakorobko@gmail.com>
2018-11-09 18:08:47 +0200

    fix counts book

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
 
   helper_method :current_order
+  before_action :categories
+
+  def categories
+    @categories = Category.with_counted_books || Category.none
+  end
 
   def current_order
     if session[:order_id]
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ApplicationRecord
   has_many :book_categories
   has_many :books, through: :book_categories
+
+  scope :with_counted_books, -> { joins(:books).select('categories.*, count(books.id) as books_count').group('categories.id') }
 end
