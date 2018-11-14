de329c4c9f4e91c12a46e2ffcc2a643acde1c64f
Roman Korobko <romakorobko@gmail.com>
2018-11-14 09:31:45 +0200

    fix some spec

--- spec/models/credit_card_spec.rb
+++ spec/models/credit_card_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe CreditCard, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:order) }
+
+  it do
+    should validate_presence_of(:credit_card_number)
+  end
+
+  it do
+    should validate_presence_of(:month)
+  end
+
+  it do
+    should validate_presence_of(:year)
+  end
+
+  it do
+    should validate_presence_of(:cvv)
+  end
+
+  it do
+    should validate_presence_of(:card_name)
+  end
+
+  it { should validate_numericality_of(:cvv).only_integer }
 end
