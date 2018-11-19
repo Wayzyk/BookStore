70b0afc9a6f44e5319e761838c07cbcfa2d07683
Roman Korobko <romakorobko@gmail.com>
2018-11-19 03:22:06 +0000

    add capybara spec for user sign in

---
+++ spec/features/user_signs_in_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+feature 'User signs in', :js => true do
+  scenario 'with valid email and password' do
+    sign_in_with 'valid@example.com', 'password'
+
+    expect(page).to have_content('Sign out')
+  end
+
+  scenario 'with invalid email' do
+    sign_in_with 'invalid_email', 'password'
+
+    expect(page).to have_content('Sign in')
+  end
+
+  scenario 'with blank password' do
+    sign_in_with 'valid@example.com', ''
+
+    expect(page).to have_content('Sign in')
+  end
+
+  def sign_in_with(email, password)
+    visit user_session_path
+    fill_in 'user_email', with: email
+    fill_in 'user_password', with: password
+    click_button 'Sign in'
+  end
+end
