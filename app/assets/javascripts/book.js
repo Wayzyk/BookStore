86ff5999ae79679ac8e6c18a2fb0a950b83fad61
Roman Korobko <romakorobko@gmail.com>
2018-11-26 08:51:06 +0000

    fix js for book

--- app/assets/javascripts/book.js
+++ app/assets/javascripts/book.js
@@ -2,14 +2,16 @@ $(document).ready(function() {
 
     var quantity = $('input.form-control.quantity-input') // Input fields for amount of books
     var amount = parseInt(quantity.val());
-    $('a>i.fa.fa-minus').click(function(){
+    $('a>i.fa.fa-minus').click(function(e){
+        e.preventDefault();
         if (amount > 1) {
             amount -= 1
             quantity.val(amount);
         }
     });
 
-    $('a>i.fa.fa-plus').click(function(){
+    $('a>i.fa.fa-plus').click(function(e){
+        e.preventDefault();
         amount += 1
         quantity.val(amount);
     });
