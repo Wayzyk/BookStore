59de4afc9174ed5b548f7ce28bd33c1e9e49dff5
Roman Korobko <romakorobko@gmail.com>
2018-11-27 08:31:33 +0000

    add validation for month

--- app/models/credit_card.rb
+++ app/models/credit_card.rb
@@ -6,8 +6,8 @@ class CreditCard < ApplicationRecord
   validates_format_of :year, with: %r{[1][1-9]}
   validates_format_of :credit_card_number, with: %r{\A\d{16}\z}
   validates_format_of :card_name, with: %r{\A[a-zA-Z\s]{0,49}\z}
-
-  def credit_card_date
-    "#{month}/#{year}"
-  end
+  validates :month, numericality: { only_integer: true,
+                                    greater_than_or_equal_to: 1,
+                                    less_than_or_equal_to: 12
+                                  }
 end
